d24e58c9009508045fcd35c55cc102d3779afefe
hozbig <hozbig@gmail.com>
2021-10-17 19:04:20 +0330

    add blog posts to home.html

--- home_page/views.py
+++ home_page/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
+from blog_articel.models import Article
 
 # home view
 def home_view(request):
+    articeles = Article.objects.all().filter(suggest=True)[:3]
     context = {
         "page_title": "خانه",
+        'articeles': articeles,
     }
     return render(request, 'home/home.html', context)
